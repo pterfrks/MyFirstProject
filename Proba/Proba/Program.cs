0dde5930daadb88358692217a6f7711d8ea1ed79
Péter Farkas <pterfrks@hotmail.com>
2019-06-24 15:36:04 +0200

    adsd beírása

--- Proba/Proba/Program.cs
+++ Proba/Proba/Program.cs
@@ -20,10 +20,8 @@ namespace Proba
             {
                 Console.WriteLine("Hello Git!");
             }
-            int a = 2;
-            int b = 3;
-            Console.WriteLine("a két szám összege: ", a+b );
-            Console.WriteLine("a két szám szorzata: ", a*b);
+            Console.WriteLine("adsd");
+
         }
     }
 }
